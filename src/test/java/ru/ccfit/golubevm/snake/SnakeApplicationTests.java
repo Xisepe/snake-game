ba50541c2b416800777201041030732ff09638c1
Maxim <maxim.golubeff2014@gmail.com>
2023-12-16 17:25:37 +0700

    init

---
+++ src/test/java/ru/ccfit/golubevm/snake/SnakeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ccfit.golubevm.snake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
